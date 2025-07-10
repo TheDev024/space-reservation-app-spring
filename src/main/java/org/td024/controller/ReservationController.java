aa20a0cb6ee05bdc952f9375f45fa465d45697e8
TheDev024 <thedeveloper024@gmail.com>
2025-07-10 15:55:04 +0400

    feat: add MapStruct-based ReservationMapper and update ReservationController to return DTOs
    - Introduced `ReservationMapper` for mapping `Reservation` entities to `ReservationDto`
    
    - Added `ReservationDto` and `NestedDto` for encapsulating reservation details
    
    - Updated `ReservationController` endpoints to return DTOs instead of entities
    
    - Enhanced project dependencies with MapStruct integration in `build.gradle`
    
    Signed-off-by: TheDev024 <thedeveloper024@gmail.com>

--- src/main/java/org/td024/controller/ReservationController.java
+++ src/main/java/org/td024/controller/ReservationController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.*;
 import org.td024.auth.entity.AppUser;
 import org.td024.dto.EditReservation;
 import org.td024.dto.MakeReservation;
+import org.td024.dto.ReservationDto;
 import org.td024.entity.Reservation;
+import org.td024.mapper.ReservationMapper;
 import org.td024.service.ReservationService;
 
 import java.util.Date;
@@ -21,42 +23,48 @@ import java.util.List;
 public class ReservationController {
 
     private final ReservationService service;
+    private final ReservationMapper reservationMapper;
 
-    public ReservationController(ReservationService service) {
+    public ReservationController(ReservationService service, ReservationMapper reservationMapper) {
         this.service = service;
+        this.reservationMapper = reservationMapper;
     }
 
     @GetMapping("/reservations")
-    public List<Reservation> getAllReservations(
+    public List<ReservationDto> getAllReservations(
             @RequestParam(required = false) Integer workspaceId,
             @RequestParam(required = false) String nameQ,
             @RequestParam(required = false) Date startTime,
             @RequestParam(required = false) Date endTime,
             @RequestParam(required = false) String username
     ) {
-        return service.getAllReservations(workspaceId, nameQ, startTime, endTime, username);
+        List<Reservation> reservations = service.getAllReservations(workspaceId, nameQ, startTime, endTime, username);
+        return reservations.stream().map(reservationMapper::toDto).toList();
     }
 
     @GetMapping("/reservations/my")
-    public List<Reservation> getAllReservations(
+    public List<ReservationDto> getAllReservations(
             @RequestParam(required = false) Integer workspaceId,
             @RequestParam(required = false) String nameQ,
             @RequestParam(required = false) Date startTime,
             @RequestParam(required = false) Date endTime,
             @AuthenticationPrincipal AppUser principal
     ) {
-        return service.getAllReservations(workspaceId, nameQ, startTime, endTime, principal.getUsername());
+        List<Reservation> reservations = service.getAllReservations(workspaceId, nameQ, startTime, endTime, principal.getUsername());
+        return reservations.stream().map(reservationMapper::toDto).toList();
     }
 
     @GetMapping("/workspaces/{workspaceId}/reservations")
-    public List<Reservation> getAllReservationsByWorkspace(@PathVariable int workspaceId, @RequestParam(required = false) String nameQ, @RequestParam(required = false) Date startTime, @RequestParam(required = false) Date endTime) {
-        return service.getAllReservations(workspaceId, nameQ, startTime, endTime, null);
+    public List<ReservationDto> getAllReservationsByWorkspace(@PathVariable int workspaceId, @RequestParam(required = false) String nameQ, @RequestParam(required = false) Date startTime, @RequestParam(required = false) Date endTime) {
+        List<Reservation> reservations = service.getAllReservations(workspaceId, nameQ, startTime, endTime, null);
+        return reservations.stream().map(reservationMapper::toDto).toList();
     }
 
     @GetMapping("/workspaces/{workspaceId}/reservations/{id}")
     @Cacheable(value = "reservations", key = "#id")
-    public Reservation getReservationById(@PathVariable int id, @PathVariable int workspaceId) {
-        return service.getReservationById(id, workspaceId);
+    public ReservationDto getReservationById(@PathVariable int id, @PathVariable int workspaceId) {
+        Reservation reservationById = service.getReservationById(id, workspaceId);
+        return reservationMapper.toDto(reservationById);
     }
 
     @ResponseStatus(HttpStatus.CREATED)
